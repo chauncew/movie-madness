54a0d9c4651b377ae702c4eadb17048ad4c25f98
Chaunce W <chauncew1137@gmail.com>
2023-07-30 15:03:37 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import MovieCard from './MovieCard';
 
 function App() {
+  const api = '3c492a64ccb984989e690cdddc623a97';
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api}`
+
+  const[movies, setMovies] = useState([]);
+  useEffect(() => {
+    // fetch(url)
+    //   .then(res => res.json())
+    //   .then(data => setMovies(data.results))
+    const apiInfo = async () => {
+      const getApi = await fetch(url);
+      const data = await getApi.json();
+      setMovies(data.results);
+      console.log(data);
+  }
+    apiInfo()
+  },[]);
+  console.log(movies)
+
   return (
     <div className="App">
       <h1 className='main-heading'>Wright's Movie Madness</h1>
       <div className="search-nav">
-
+        <div>
+          <form>
+            <input type="text" placeholder='Enter A Movie'/>
+            <button>Search Madness</button>
+          </form>
+       </div>
       </div>
-      <div>
-        <form>
-          <input type="text" placeholder='Enter A Movie'/>
-          <button>Search</button>
-        </form>
+      
+
+      <div className="movies">
+        {movies.map((movie) => {
+          <MovieCard />
+        })}
       </div>
     </div>
   );
