44e38f9c8abbfc3da03179e9ff3359b3084ab1cb
Chaunce W <chauncew1137@gmail.com>
2023-07-30 10:43:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='main-heading'>Wright's Movie Madness</h1>
+      <div className="search-nav">
+
+      </div>
+      <div>
+        <form>
+          <input type="text" placeholder='Enter A Movie'/>
+          <button>Search</button>
+        </form>
+      </div>
     </div>
   );
 }
