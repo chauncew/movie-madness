6383b56525c72fa5057b1d4c670846a14b8bcf0e
Chaunce W <chauncew1137@gmail.com>
2023-08-04 13:56:10 -0500

    Site is almost done and ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,44 +1,117 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import MovieCard from './MovieCard';
+import gsap from "gsap";
+import clapperImage from './srcImages/clapper.png';
+import movieReel from './srcImages/movie-reel.png';
+
 
 function App() {
-  const api = '3c492a64ccb984989e690cdddc623a97';
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api}`
+  const apiKey = '3c492a64ccb984989e690cdddc623a97';
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+  const urlSearch = `https://api.themoviedb.org/3/search/movie?&api_key=${apiKey}&query=`;
+  const headingRef = useRef(null);
+  const clapperRef = useRef(null);
+  const searchRef = useRef(null);
+  const reelsRef1 = useRef(null);
+  const reelsRef2 = useRef(null);
+
+ 
 
   const[movies, setMovies] = useState([]);
+  const[search, setSearch] = useState("");
+  const[clear, setClear] = useState("");
+
   useEffect(() => {
-    // fetch(url)
-    //   .then(res => res.json())
-    //   .then(data => setMovies(data.results))
-    const apiInfo = async () => {
+    const api = async () => {
       const getApi = await fetch(url);
       const data = await getApi.json();
       setMovies(data.results);
-      console.log(data);
   }
-    apiInfo()
+    api()
   },[]);
-  console.log(movies)
+
+  const movieSearch = (e) => {
+    e.preventDefault()
+    const searchApi = async () => {
+      const apiSearch = await fetch(urlSearch + search)
+    const searchData = await apiSearch.json()
+    setMovies(searchData.results)
+  }
+    setSearch();
+    // console.log(searchData);
+    searchApi();
+  };
+
+
+
+  //Heading Animation
+   useEffect(() => {
+      const headingAnim = headingRef.current;
+      gsap.from(headingAnim, {x:-1800, duration: 1, delay: 1});
+  },[]);
+
+  //Search Box Animations
+   useEffect(() => {
+      const searchAnim = searchRef.current;
+      gsap.from(searchAnim, {y:-500, duration: 1, delay: 1.8});
+  },[]);
+
+   //Clapper Animation
+  useEffect(() => {
+      const clapperAnim = clapperRef.current;
+      gsap.fromTo(clapperAnim, {scale: 0 , opacity: 0}, {scale: 1, opacity: 1, duration: 1.5, delay: 3});
+      setTimeout(() => {
+        gsap.fromTo(clapperAnim, {scale: 1 , opacity: 1}, {scale: 0, opacity: 0, duration: 1.5});
+      },4500)
+  },[]);
+
+  //Movie Reels Animations
+  useEffect(() => {
+    const reelsAnim = reelsRef1.current;
+    gsap.from(reelsAnim, {opacity: 0, duration: 1, delay: 5.8});
+},[]);
+
+  useEffect(() => {
+    const reelsAnim = reelsRef2.current;
+    gsap.from(reelsAnim, {opacity: 0, duration: 1, delay: 5.8});
+},[]);
 
   return (
     <div className="App">
-      <h1 className='main-heading'>Wright's Movie Madness</h1>
+      <h1 ref={headingRef} className='main-heading'>Wright's Movie Madness</h1>
       <div className="search-nav">
-        <div>
-          <form>
-            <input type="text" placeholder='Enter A Movie'/>
-            <button>Search Madness</button>
-          </form>
-       </div>
+      <form ref={searchRef} onSubmit={movieSearch} value={search}>
+        <input onChange={(e) => setSearch(e.target.value)}
+          type="text" placeholder='Enter A Movie'/>
+          <button>Search</button>
+        </form>
       </div>
-      
 
-      <div className="movies">
-        {movies.map((movie) => {
-          <MovieCard />
-        })}
+      <div ref={clapperRef} className="clapper-image">
+        <img src={clapperImage} alt="" />
+        <h2>Action</h2>
       </div>
+
+      <div ref={reelsRef1} className="movie-reel movie-reel1">
+        <img src={movieReel} alt="" />
+      </div>
+
+      <div ref={reelsRef2} className="movie-reel movie-reel2">
+        <img src={movieReel} alt="" />
+      </div>
+
+      <div>
+        {movies.length > 0 ?(
+          <div className='movies'>
+            {movies.map((movie)=> 
+            <MovieCard key={movie.id} {...movie}/>)}
+          </div>
+        ):(
+        <h2 className='not-found'>No Movie Entered/Found<br/>Please Try Again And Movies Will Be Loaded In 5 Seconds</h2>
+        
+         )}
+      </div>  
     </div>
   );
 }
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,32 +2,38 @@ import React, { useEffect, useRef } from 'react';
 import gsap from "gsap";
 
 
+
 const MovieCard = ({title, vote_average, poster_path, overview}) => {
   const api_image = 'https://image.tmdb.org/t/p/w500/';
-  const cardRef = useRef(null);
 
-  //Card Animation
-  useEffect(() => {
-      const animation = cardRef.current;
-      gsap.fromTo(animation, {scale: 0 , opacity: 0}, {scale: 1, opacity: 1, ease: 'bounce.out',
-      duration: 1.5, delay: 6});
-  },[]);
+  const loadingRef = useRef(null)
+
+  //Loading Animation
+  useEffect (() => {
+    const loadingAnim = loadingRef.current;
+    gsap.fromTo(loadingAnim, {scale: 0 }, {scale: 1, opacity: 1, duration: 1.5, delay: 4.6, autoAlpha: 0});
+  },[])
+   
 
   return (
-    <div className='card' ref={cardRef}> 
-        <div className='card-img'>
-            <img src={poster_path ? api_image + poster_path : "https://img.freepik.com/free-vector/empty-cinema-hall-realistic-background_1284-11758.jpg?size=626&ext=jpg&uid=R21887356&ga=GA1.1.1729684245.1687615386&semt=ais" } alt="" />
-          <div className="overview">
-            <h2 className="title-overview">Overview</h2>
-            <h3 className='overview-info'>{overview}</h3>
-          </div>
-        </div>
-
-      <div className="info">
-        <p className="title">{title}</p>
-        <p className="vote">Rating {vote_average}</p>
+    <div>
+      <div className="loading" ref={loadingRef}>
+          <h1>Loading &#128534;&#128534;&#128534;&#129324;</h1>
       </div>
+    <div className='card'> 
+        <div className='cards'>
+          <img src={poster_path ? api_image + poster_path : "https://img.freepik.com/free-vector/empty-cinema-hall-realistic-background_1284-11758.jpg?size=626&ext=jpg&uid=R21887356&ga=GA1.1.1729684245.1687615386&semt=ais" } alt="" />
+          <div className="info">
+            <p className="title">{title}</p>
+            <p className="vote">Rating {vote_average}</p>
+        </div>
+      </div>
+        <div className="overview">
+          <h2 className="title-overview">Overview</h2>
+          <h3 className='overview-info'>{overview}</h3>
+        </div>
     </div>
+  </div>
   );
 };
 
